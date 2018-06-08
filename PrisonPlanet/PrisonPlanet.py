c09a663eac339ead4c209f54ce4def1a49c6975f
Bret Williams <straightener@hotmail.com>
2018-06-08 17:02:44 -0500

    Input and functions

--- PrisonPlanet/PrisonPlanet.py
+++ PrisonPlanet/PrisonPlanet.py
@@ -1,1 +1,19 @@
+def play():
+    print("Escape the Prison Planet!")
+    action_input = get_player_command()
+    if action_input == 'n':
+        print("Go North")
+    elif action_input == 's':
+        print("Go South")
+    elif action_input == 'e':
+        print("Go East")
+    elif action_input == 'w':
+        print("Go West")
+    else:
+        print("Invalid action")
 
+
+def get_player_command():
+    return input('Action: ').lower()
+
+play()
