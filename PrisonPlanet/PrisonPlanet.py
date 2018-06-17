d648c5400a9b0c40af06aed9db2f5fd23f818e11
Bret Williams <straightener@hotmail.com>
2018-06-17 16:50:36 -0500

    Ch 7

--- PrisonPlanet/PrisonPlanet.py
+++ PrisonPlanet/PrisonPlanet.py
@@ -1,19 +1,28 @@
-def play():
-    print("Escape the Prison Planet!")
-    action_input = get_player_command()
-    if action_input == 'n':
-        print("Go North")
-    elif action_input == 's':
-        print("Go South")
-    elif action_input == 'e':
-        print("Go East")
-    elif action_input == 'w':
-        print("Go West")
-    else:
-        print("Invalid action")
-
-
 def get_player_command():
     return input('Action: ').lower()
+
+def play():
+    inventory = ['Dagger','Gold(5)','Crusty Bread']
+    print("Escape the Prison Planet!")
+    while True:
+        action_input = get_player_command()
+        if action_input == 'n':
+            print("Go North")
+        elif action_input == 's':
+            print("Go South")
+        elif action_input == 'e':
+            print("Go East")
+        elif action_input == 'w':
+            print("Go West")
+        elif action_input == 'i':
+            print("Inventory: ")
+            pretty_print_unordered(inventory)
+        else:
+            print("Invalid action")
+
+def pretty_print_unordered(to_print):
+    for item in to_print:
+        print("* " + str(item))
+
 
 play()
