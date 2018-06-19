09c1106687897ec7314b55d24aed667c3ecf157a
Bret Williams <straightener@hotmail.com>
2018-06-18 19:23:27 -0500

    chapter 8

--- PrisonPlanet/PrisonPlanet.py
+++ PrisonPlanet/PrisonPlanet.py
@@ -1,8 +1,32 @@
+class Weapon:    #note that a superclass has to be above the subclass...ugh!
+    def __str__(self):
+        return self.name
+
+class Crysknife(Weapon):
+    def __init__(self):
+        self.name = "Crysknife"
+        self.description = "A deadly blade carved from a tooth of Shai Hulud."
+        self.damage = 5
+
+class Lasgun(Weapon):
+    def __init__(self):
+        self.name = "Lasgun"
+        self.description = "Now this is what I call a weapon, " \
+            "just point and watch the fun."
+        self.damage = 20
+
+class StunBaton(Weapon):
+    def __init__(self):
+        self.name = "Stun Baton"
+        self.description = "A short staff with a cone projector at one end. " \
+            "Causes painful blisters when in range."
+        self.damage = 10
+
 def get_player_command():
     return input('Action: ').lower()
 
 def play():
-    inventory = ['Dagger','Gold(5)','Crusty Bread']
+    inventory = [Crysknife(),'Gold(5)','Crusty Bread']
     print("Escape the Prison Planet!")
     while True:
         action_input = get_player_command()
@@ -17,6 +41,8 @@ def play():
         elif action_input == 'i':
             print("Inventory: ")
             pretty_print_unordered(inventory)
+        elif action_input == 'q':     #quit
+            break
         else:
             print("Invalid action")
 
